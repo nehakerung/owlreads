cfe105a2da81a4c753199949c81a78a563cf9b87
Neha Kerung <nehakerung@gmail.com>
2026-01-18 16:57:49 +0000

    Delete api directory
    Google books api falls in book dir

--- core/api/serializers.py
+++
@@ -1,9 +0,0 @@
-# serialises
-from books.models import Book
-from rest_framework import serializers
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
