76e62a68a19b098e61a12496a783f31951a79ceb
Neha Kerung <nehakerung@gmail.com>
2026-01-21 17:25:44 +0000

    Fix files, add font info

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-[#9dcd5a] py-6 border-b">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        
+
         {/* Logo */}
         <div className="flex items-center gap-6">
           <Link href="/">
