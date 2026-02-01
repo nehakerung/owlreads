3cb15f9595000f50bdd3fedff4fb189d6dcd82fd
Neha Kerung <nehakerung@gmail.com>
2026-02-01 15:38:35 +0000

    Add books to Home page
    Section for books

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
 import Section from '../components/home/Section'
+import BookSection from '../components/home/BookSection'
 
 const HomePage = () => {
   return (
     <>
     <Hero />
     <Section />
+    <BookSection />
     </>
   )
 }
--- frontend/src/components/home/BookSection.tsx
+++ frontend/src/components/home/BookSection.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Card from './Card'
+import BookCard from './BookCard'
 
-const Section = () => {
+const BookSection = () => {
   return (
     <section className="main-max-width padding-x mx-auto">
       <h2 className="my-9 text-center text-xl font-bold text-gray-800">
-        Browse By
+        Find featured books
       </h2>
 
       {/* Content */}
       <div className="flex justify-center flex-wrap gap-8">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
 
       </div>
     </section>
   )
 }
 
-export default Section
+export default BookSection
