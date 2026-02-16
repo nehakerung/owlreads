ae4bd59cf255805306f19b5e7c3eb2692cf225e1
Neha Kerung <nehakerung@gmail.com>
2026-02-16 11:42:42 +0000

    Add prettier

--- frontend/src/app/personal/bookshelf/books.tsx
+++ frontend/src/app/personal/bookshelf/books.tsx
@@ -11,8 +11,11 @@ const Books = () => {
 
       <div className="flex flex-wrap gap-6 lg:gap-8 justify-between w-full">
         <div className="w-150 max-lg:w-full border border-gray-200 shadow-sm rounded-lg bg-white overflow-hidden flex-1">
-          <div className="max-h-150 overflow-y-auto px-6 py-4">
-            <BookItemLong />
+             <div className="max-h-150 overflow-y-auto px-6 py-4">
+                <BookItemLong />
+
+
+
             <BookItemLong />
             <BookItemLong />
           </div>
