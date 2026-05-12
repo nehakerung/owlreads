d8c447f121c14790c98337dc6b9b5186b9a00eae
Neha Kerung <nehakerung@gmail.com>
2026-05-12 13:40:07 +0100

    Remove redundant code

--- core/books/tests.py
+++ core/books/tests.py
@@ -3,21 +3,28 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from .models import Book
+
 
 class BookAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.book = Book.objects.create(
+            google_books_id="test-google-id",
+            title="Test Book",
+            authors=["Test Author"],
+        )
 
-    def test_book_search(self):
-        url = reverse('book-search')
-        response = self.client.get(url, {'q': 'django'})
+    def test_book_list_search(self):
+        url = reverse("book-list")
+        response = self.client.get(url, {"q": "Test"})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
 
     def test_book_detail(self):
-        url = reverse('book-detail', kwargs={'book_id': 'zjnwDwAAQBAJ'})
+        url = reverse("book-detail", kwargs={"pk": self.book.pk})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('title', response.data)
+        self.assertIn("title", response.data)
